a8fe8c4eec3698fd392933a0e141a372d766c3eb
William MacDonald <wmsmacdonald@gmail.com>
2016-08-22 00:54:57 -0500

    functions

--- test/test.js
+++ test/test.js
@@ -1,2 +1,31 @@
 'use strict';
 
+const assert = require('chai').assert;
+const vcd = require('vcdiff');
+
+const vcdiffDecoder = require('../');
+
+describe('vcdiff', function() {
+  let source = new Buffer('version number 1');
+  let target = new Buffer('version number 2');
+  let hashedSource = new vcd.HashedDictionary(source);
+  let delta = vcd.vcdiffEncodeSync(target, { hashedDictionary: hashedSource });
+  console.log(delta.toString());
+
+  describe('#decodeSync', function() {
+    it('should return the correct target', function() {
+
+      let decodedTarget = vcdiffDecoder.decodeSync(delta, source);
+      // make sure decoded is same as target
+      assert.strictEqual(decodedTarget.toString(), target.toString());
+    });
+  });
+  describe('#decode', function() {
+    it('should return a promise that resolves to the correct target', function(done) {
+      vcdiffDecoder.decode(delta, source).then((err, decodedTarget) => {
+        assert.strictEqual(decodedTarget.toString(), target.toString());
+        done();
+      });
+    });
+  });
+});
