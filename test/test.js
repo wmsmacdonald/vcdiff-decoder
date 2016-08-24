2ff830966a75b66e3ce3bb0f176822b19ddd439c
William MacDonald <wmsmacdonald@gmail.com>
2016-08-24 00:50:39 -0500

    consume linked integers

--- test/test.js
+++ test/test.js
@@ -4,13 +4,15 @@ const assert = require('chai').assert;
 const vcd = require('vcdiff');
 
 const vcdiffDecoder = require('../');
+const VCDiff = require('../lib/vcdiff');
 
-describe('vcdiff', function() {
+/*describe('vcdiffDecoder', function() {
   let source = new Buffer('test 1');
   let target = new Buffer('test 2');
   let hashedSource = new vcd.HashedDictionary(source);
   let delta = vcd.vcdiffEncodeSync(target, { hashedDictionary: hashedSource });
   //console.log(delta.toString());
+  console.log(delta);
 
   describe('#decodeSync', function() {
     it('should return the correct target', function() {
@@ -26,6 +28,16 @@ describe('vcdiff', function() {
         assert.strictEqual(decodedTarget.toString(), target.toString());
         done();
       });
+    });
+  });
+});*/
+
+describe('vcdiff', function() {
+  describe('#_consumeInteger', function() {
+    it('should return the integer value', function() {
+      let delta = new Buffer([0x56, 0x43, 0x44, 0x0, 0x0, 0xBA, 0xEF, 0x9A, 0x15]);
+      let vcdiff = new VCDiff(delta);
+      assert.strictEqual(vcdiff._consumeInteger(), 123456789);
     });
   });
 });
