26b3ba4ab549569f2af3c0959f6f0d6f02328cdf
William MacDonald <wmsmacdonald@gmail.com>
2016-08-25 15:30:45 -0500

    more tests

--- test/test.js
+++ test/test.js
@@ -4,6 +4,7 @@ const assert = require('chai').assert;
 const vcd = require('vcdiff');
 
 const vcdiffDecoder = require('../');
+const errors = require('../lib/errors');
 const VCDiff = require('../lib/vcdiff');
 
 /*describe('vcdiffDecoder', function() {
@@ -33,11 +34,54 @@ const VCDiff = require('../lib/vcdiff');
 });*/
 
 describe('vcdiff', function() {
+  describe('#_consumeHeader', function() {
+    it('should move position up to 5', function () {
+      let delta = new Buffer([0x56, 0x43, 0x44, 0x0, 0x0]);
+      let vcdiff = new VCDiff(delta);
+      vcdiff._consumeHeader();
+      assert.strictEqual(vcdiff.position, 5);
+    });
+    it('should throw an error if not a valid vcdiff', function () {
+      let delta = new Buffer([0x56, 0x42, 0x44, 0x0, 0x0]);
+      let vcdiff = new VCDiff(delta);
+      try {
+        vcdiff._consumeHeader()
+      }
+      catch (e) {
+        assert(e instanceof errors.InvalidDelta);
+      }
+    });
+    it('should throw an error if hdr_indicator is non-zero', function () {
+      let delta = new Buffer([0x56, 0x43, 0x44, 0x0, 0x1]);
+      let vcdiff = new VCDiff(delta);
+      try {
+        vcdiff._consumeHeader();
+        assert.fail();
+      }
+      catch (e) {
+        assert.strictEqual(e.name, 'NotImplemented');
+      }
+    });
+  });
+  describe('#position', function() {
+    it('starts at zero', function() {
+      let delta = new Buffer([0xBA, 0xEF, 0x9A, 0x15]);
+      let vcdiff = new VCDiff(delta);
+      assert.strictEqual(vcdiff.position, 0);
+    })
+  });
   describe('#_consumeInteger', function() {
-    it('should return the integer value', function() {
-      let delta = new Buffer([0x56, 0x43, 0x44, 0x0, 0x0, 0xBA, 0xEF, 0x9A, 0x15]);
+    it('should return the correct value from a multi byte integer', function() {
+      // convert VCDIFF linked integer to base10
+      let delta = new Buffer([0xBA, 0xEF, 0x9A, 0x15]); // 58,
       let vcdiff = new VCDiff(delta);
       assert.strictEqual(vcdiff._consumeInteger(), 123456789);
+    });
+    it('should return the correct value from a single byte integer', function() {
+      // convert VCDIFF linked integer to base10
+      let delta = new Buffer([43]);
+      let vcdiff = new VCDiff(delta);
+      assert.strictEqual(vcdiff._consumeInteger(), 43);
     });
   });
 });
