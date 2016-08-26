179dd2a019fe6741cb1aa0267e710547aba32747
William MacDonald <wmsmacdonald@gmail.com>
2016-08-26 00:22:30 -0500

    parsing delta window

--- test/test.js
+++ test/test.js
@@ -6,6 +6,8 @@ const vcd = require('vcdiff');
 const vcdiffDecoder = require('../');
 const errors = require('../lib/errors');
 const VCDiff = require('../lib/vcdiff');
+const Deserialize = require('../lib/deserialize');
+const vcdiffTargetWindow = require('../lib/vcdiff_target_window');
 
 /*describe('vcdiffDecoder', function() {
   let source = new Buffer('test 1');
@@ -70,18 +72,47 @@ describe('vcdiff', function() {
       assert.strictEqual(vcdiff.position, 0);
     })
   });
-  describe('#_consumeInteger', function() {
+
+});
+
+describe('Deserialize', function() {
+  describe('#integer', function() {
     it('should return the correct value from a multi byte integer', function() {
       // convert VCDIFF linked integer to base10
       let delta = new Buffer([0xBA, 0xEF, 0x9A, 0x15]); // 58,
-      let vcdiff = new VCDiff(delta);
-      assert.strictEqual(vcdiff._consumeInteger(), 123456789);
+      let { nextPosition, value } = Deserialize.integer(delta, 0);
+      assert.strictEqual(value, 123456789);
+      assert.strictEqual(nextPosition, 4);
     });
     it('should return the correct value from a single byte integer', function() {
       // convert VCDIFF linked integer to base10
       let delta = new Buffer([43]);
-      let vcdiff = new VCDiff(delta);
-      assert.strictEqual(vcdiff._consumeInteger(), 43);
+      let { nextPosition, value } = Deserialize.integer(delta, 0);
+      assert.strictEqual(value, 43);
+      assert.strictEqual(nextPosition, 1);
+    });
+  });
+});
+
+describe('vcdiffTargetWindow', function() {
+  describe('.FromSource', function() {
+    describe('#decode', function () {
+      it('should return the correct value from a multi byte integer', function () {
+        let source = new Buffer('test 1\n');
+        let target = new Buffer('test 2\n');
+        let hashedSource = new vcd.HashedDictionary(source);
+        let delta = vcd.vcdiffEncodeSync(target, { hashedDictionary: hashedSource });
+
+
+        //let delta = new Buffer([0x0d, 0x07, 0x00, 0x07, 0x01, 0x00, 0x74, 0x65, 0x73, 0x74, 0x20, 0x32, 0xa]);
+        let targetWindow = new vcdiffTargetWindow.FromSource(delta, 8);
+        assert.strictEqual(JSON.stringify(targetWindow.parse()), JSON.stringify({
+          nextPosition: 22,
+          data: new Buffer('test 2\n'),
+          instructions: new Buffer([0x08]),
+          addresses: new Buffer([])
+        }));
+      });
     });
   });
 });
