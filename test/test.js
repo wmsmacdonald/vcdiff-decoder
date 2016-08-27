39251357888fec2bb947a3a6e0993ebf8d7a3ad7
William MacDonald <wmsmacdonald@gmail.com>
2016-08-26 21:26:34 -0500

    passing test for simple instruction

--- test/test.js
+++ test/test.js
@@ -92,26 +92,88 @@ describe('Deserialize', function() {
       assert.strictEqual(nextPosition, 1);
     });
   });
+  describe('#winIndicator', function() {
+    // TODO parameterize these
+    it('value of 0', function() {
+      let winIndicator = Deserialize.winIndicator(0);
+      assert.strictEqual(winIndicator.VCD_SOURCE, 0);
+      assert.strictEqual(winIndicator.VCD_TARGET, 0);
+    });
+    it('value of 1', function() {
+      let winIndicator = Deserialize.winIndicator(1);
+      assert.strictEqual(winIndicator.VCD_SOURCE, 1);
+      assert.strictEqual(winIndicator.VCD_TARGET, 0);
+    });
+    it('value of 2', function() {
+      let winIndicator = Deserialize.winIndicator(2);
+      assert.strictEqual(winIndicator.VCD_SOURCE, 0);
+      assert.strictEqual(winIndicator.VCD_TARGET, 1);
+    });
+    it('value of 3', function() {
+      let winIndicator = Deserialize.winIndicator(3);
+      assert.strictEqual(winIndicator.VCD_SOURCE, 1);
+      assert.strictEqual(winIndicator.VCD_TARGET, 1);
+    });
+  });
+  describe('#hdrIndicator', function() {
+    it('value of 0', function() {
+      let winIndicator = Deserialize.hdrIndicator(0);
+      assert.strictEqual(winIndicator.VCD_DECOMPRESS, 0);
+      assert.strictEqual(winIndicator.VCD_CODETABLE, 0);
+    });
+    it('value of 1', function() {
+      let winIndicator = Deserialize.hdrIndicator(1);
+      assert.strictEqual(winIndicator.VCD_DECOMPRESS, 1);
+      assert.strictEqual(winIndicator.VCD_CODETABLE, 0);
+    });
+    it('value of 2', function() {
+      let winIndicator = Deserialize.hdrIndicator(2);
+      assert.strictEqual(winIndicator.VCD_DECOMPRESS, 0);
+      assert.strictEqual(winIndicator.VCD_CODETABLE, 1);
+    });
+    it('value of 3', function() {
+      let hdrIndicator = Deserialize.hdrIndicator(3);
+      assert.strictEqual(hdrIndicator.VCD_DECOMPRESS, 1);
+      assert.strictEqual(hdrIndicator.VCD_CODETABLE, 1);
+    });
+  });
 });
 
 describe('vcdiffTargetWindow', function() {
   describe('.FromSource', function() {
-    describe('#decode', function () {
-      it('should return the correct value from a multi byte integer', function () {
+    describe('#parse', function () {
+      it('should return a correct object with data, instructions, and addresses', function () {
         let source = new Buffer('test 1\n');
         let target = new Buffer('test 2\n');
         let hashedSource = new vcd.HashedDictionary(source);
         let delta = vcd.vcdiffEncodeSync(target, { hashedDictionary: hashedSource });
+        let deltaDeserialized, targetWindow;
 
 
         //let delta = new Buffer([0x0d, 0x07, 0x00, 0x07, 0x01, 0x00, 0x74, 0x65, 0x73, 0x74, 0x20, 0x32, 0xa]);
-        let targetWindow = new vcdiffTargetWindow.FromSource(delta, 8);
-        assert.strictEqual(JSON.stringify(targetWindow.parse()), JSON.stringify({
-          nextPosition: 22,
+        targetWindow = new vcdiffTargetWindow.FromSource(delta, 8, source, 0x07, 0x00);
+        deltaDeserialized = targetWindow._parse();
+        assert.strictEqual(JSON.stringify(deltaDeserialized), JSON.stringify({
+          targetWindowLength: 7,
           data: new Buffer('test 2\n'),
           instructions: new Buffer([0x08]),
           addresses: new Buffer([])
         }));
+        assert.strictEqual(targetWindow.position, 22);
+      });
+    });
+    describe('#decode', function () {
+      it('should return a correct target buffer', function () {
+        let source = new Buffer('test 1\n');
+        let target = new Buffer('test 2\n');
+        let hashedSource = new vcd.HashedDictionary(source);
+        let delta = vcd.vcdiffEncodeSync(target, { hashedDictionary: hashedSource });
+        let deltaDeserialized, targetWindow;
+        targetWindow = new vcdiffTargetWindow.FromSource(delta, 8, source, 0x07, 0x00);
+
+        let { position, target: constructedTarget } = targetWindow.decode();
+        assert.strictEqual(position, 22);
+        assert.strictEqual(constructedTarget.toString('ascii'), target.toString('ascii'));
       });
     });
   });
